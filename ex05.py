2de7e0250b10eb2cb85c8c31cf9a822b63ca5097
Bianca Almeida <151306147+biancalmds@users.noreply.github.com>
2025-08-15 08:33:51 -0300

    Refeito o código do ex05.py

--- ex05.py
+++ ex05.py
@@ -1,46 +1,32 @@
-menu = {
+cardapio = {
     100: ("Cachorro Quente", 1.20),
     101: ("Bauru Simples", 1.30),
     102: ("Bauru com Ovo", 1.50),
     103: ("Hambúrguer", 1.20),
     104: ("Cheeseburguer", 1.30),
-    105: ("Refrigerante", 1.00),
+    105: ("Refrigerante", 1.00)
 }
 
-print("Lanchonete")
-print("Digite o código do item e a quantidade.")
-print("Aperte e 'ENTER' para encerrar o pedido.\n")
-
-pedido = [] 
+pedido = {
+    100: 0,
+    101: 0,
+    102: 0,
+    103: 0,
+    104: 0,
+    105: 0
+}
 
 while True:
-    cod_txt = input("Código do item (ENTER para encerrar): ").strip()
-    if cod_txt == "":
+    codigo = int(input("Digite o código do item desejado: "))
+    quantidade = int(input(f"Digite a quantidade de {cardapio[codigo][0]} desejada: "))
+    pedido[codigo] += (cardapio[codigo][1] * quantidade)
+    continuar = input("Deseja continuar comprando? (S/N): ").upper()
+    if continuar == "N":
         break
-    if not cod_txt.isdigit() or int(cod_txt) not in menu:
-        print(">> Código inválido. Tente de novo.\n")
-        continue
 
-    cod = int(cod_txt)
-    nome, preco = menu[cod]
+print("\nVALOR A SER PAGO POR ITEM: ")
+for codigo, preco in pedido.items():
+    if preco != 0:
+       print(f"- {cardapio[codigo][0]}: R${preco:.2f}")
 
-    try:
-        qtd = int(input(f"Quantidade de '{nome}': ").strip())
-        if qtd <= 0:
-            print(">> Quantidade deve ser positiva.\n")
-            continue
-    except ValueError:
-        print(">> Quantidade inválida.\n")
-        continue
-
-    subtotal = preco * qtd
-    pedido.append((nome, preco, qtd, subtotal))
-    print(f"Adicionado: {nome} x{qtd}  -> R$ {subtotal:.2f}\n")
-
-print("\n CUPOM FISCAL")
-total = 0.0
-for nome, preco, qtd, subtotal in pedido:
-    total += subtotal
-    print(f"{nome:16} {qtd:>3} x R$ {preco:>4.2f} = R$ {subtotal:>6.2f}")
-print("-" * 38)
-print(f"TOTAL: R$ {total:>6.2f}")
+print(f"\nTOTAL GERAL DO PEDIDO: R${sum(list(pedido.values())):.2f}")
